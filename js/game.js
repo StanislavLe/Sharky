72dbc73cfffc67d337a93ba913c11a83ab76de23
Stanislav Levin <stanislav2708@outlook.de>
2025-02-14 09:28:27 +0100

    add models

---
+++ js/game.js
@@ -0,0 +1,9 @@
+let canvas;
+let ctx;
+let character = new movableObject();
+
+function init() {
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+console.log('My character is', character)
+}
