32b19fc1e77b02290aa81ba511f027c3eca9c539
Stanislav Levin <stanislav2708@outlook.de>
2025-02-19 19:25:12 +0100

    SHARKY Animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,16 +1,37 @@
-class Character extends MovableObject{
-height = 300;
-width = 300;
-y = 200;
-x = 0;
+class Character extends MovableObject {
+    height = 300;
+    width = 300;
+    y = 200;
+    x = 0;
+    IMAGES_WALKING = [
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/1.png',
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/2.png',
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/3.png',
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/4.png',
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/5.png',
+        'img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/6.png'
+    ]
+    currentImage = 0;
+
+
     constructor() {
         super().loadImage('img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/3.Swim/1.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
     }
 
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000/6)
+    }
 
     jump() {
-        
+
     }
 
-    
+
 }
